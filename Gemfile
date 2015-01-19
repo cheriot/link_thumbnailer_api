8467f8f48e7a257a76cf4e840a44160e050d8568
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2015-01-19 22:57:48 +0100

    optimize stack

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.2.0'
 gem 'rails-api'
+gem 'thin'
 gem 'responders', '~> 2.0'
 
 gem 'link_thumbnailer'
--- config/application.rb
+++ config/application.rb
@@ -4,11 +4,11 @@ require File.expand_path('../boot', __FILE__)
 # require "active_model/railtie"
 # require "active_job/railtie"
 # require "active_record/railtie"
-require "action_controller/railtie"
+require 'action_controller/railtie'
 # require "action_mailer/railtie"
-require "action_view/railtie"
+# require 'action_view/railtie'
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,5 +27,8 @@ module LinkThumbnailerApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.delete ::Rack::ConditionalGet
+    config.middleware.delete ::Rack::ETag
   end
 end
